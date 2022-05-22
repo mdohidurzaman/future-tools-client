e6fc722ac043810a049bfd8f2168dcbaf8e0d7a8
Md. Ohidurzaman <mozaman.knit@gmail.com>
2022-05-22 22:30:13 +0600

    Added firebase auth

---
+++ src/Components/Shared/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p className="footer-text">©Copyright 2022 Furture Tools.</p>
+    </div>
+  );
+};
+
+export default Footer;
